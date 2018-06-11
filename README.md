9506f238317a670319e2964f12344231164ba5a7
mykola <mykola>
2018-06-11 19:37:43 +0300

    Initial commit. The core functionality of analyzer is implemented. There are a number of TODO items to complete: 1) Make the "Method_with_same_type_params_invoked_with_vars_named_same_as_args_does_not_trigger_diagnostic" test pass. 2) The corresponding code-fix. See `CSharpUseNamedArgumentsCodeRefactoringProvider` in https://github.com/dotnet/roslyn for code sample to start from. 3) Create an analyzer that      - requires using named arguments if the invoked method is marked with a special attribute or comment;      - does not analyze anything else;      - shares as much code as possible with `UseNamedArgsForParamsOfSameTypeAnalyzer`.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Use named arguments for invocations of methods with multiple parameters of the same type.
+This is work in progress...
+
+![The UseNamedArguments analyzer in action](./use-named-args-demo.gif)
+
+# Thank you!
+
+- [Dustin Campbell](https://github.com/DustinCampbell) for [CSharpEssentials](https://github.com/DustinCampbell/CSharpEssentials)
+
+# License
+
+The [UseNamedArguments](https://github.com/mykolav/use-named-arguments) analyzer and code-fix are licensed under the MIT license.  
+So they can be used freely in commercial applications.
---
+++ UseNamedArguments/UseNamedArgsForParamsOfSameTypeAnalyzer.cs
@@ -0,0 +1,143 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using UseNamedArguments.Support;
+
+namespace UseNamedArguments
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class UseNamedArgsForParamsOfSameTypeAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "UseNamedArguments";
+
+        // You can change these strings in the Resources.resx file.
+        // If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
+        // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
+        private static readonly LocalizableString Title = new LocalizableResourceString(
+            nameof(Resources.AnalyzerTitle), 
+            Resources.ResourceManager, 
+            typeof(Resources));
+
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(
+            nameof(Resources.AnalyzerMessageFormat), 
+            Resources.ResourceManager, 
+            typeof(Resources));
+
+        private static readonly LocalizableString Description = new LocalizableResourceString(
+            nameof(Resources.AnalyzerDescription), 
+            Resources.ResourceManager, 
+            typeof(Resources));
+
+        private const string Category = "Naming";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+            DiagnosticId, 
+            Title, 
+            MessageFormat, 
+            Category, 
+            DiagnosticSeverity.Warning, 
+            isEnabledByDefault: true, 
+            description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
+            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
+            // context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+            context.RegisterSyntaxNodeAction(
+                AnalyzeInvocationOrObjectCreationExpressionNode,
+                SyntaxKind.InvocationExpression,
+                SyntaxKind.ObjectCreationExpression);
+        }
+
+        private class ArgumentSyntaxAndParameterSymbol
+        {
+            public ArgumentSyntaxAndParameterSymbol(ArgumentSyntax argument, IParameterSymbol parameter)
+            {
+                Argument = argument;
+                Parameter = parameter;
+            }
+
+            public ArgumentSyntax Argument { get; }
+            public IParameterSymbol Parameter { get; }
+        }
+
+        public void AnalyzeInvocationOrObjectCreationExpressionNode(SyntaxNodeAnalysisContext context)
+        {
+            var semanticModel = context.SemanticModel;
+            var invocationExpressionSyntax = (InvocationExpressionSyntax)context.Node;
+
+            var methodSymbol = semanticModel.GetSymbolInfo(invocationExpressionSyntax).Symbol as IMethodSymbol;
+            if (methodSymbol == null)
+                return;
+
+            if (!methodSymbol.MethodKind.In(
+                    MethodKind.Ordinary, 
+                    MethodKind.Constructor, 
+                    MethodKind.LocalFunction))
+            {
+                return;
+            }
+
+            var argumentSyntaxesByType = new Dictionary<ITypeSymbol, List<ArgumentSyntaxAndParameterSymbol>>();
+            foreach (var argumentSyntax in invocationExpressionSyntax.ArgumentList.Arguments)
+            {
+                var argumentInfo = semanticModel.GetArgumentInfo(argumentSyntax);
+                if (argumentInfo.IsEmpty)
+                    throw new InvalidOperationException($"Could not find the corresponding parameter for [{argumentSyntax}]");
+
+                if (!argumentSyntaxesByType.TryGetValue(argumentInfo.Parameter.Type, out var argumentSyntaxes))
+                {
+                    argumentSyntaxes = new List<ArgumentSyntaxAndParameterSymbol>();
+                    argumentSyntaxesByType.Add(argumentInfo.Parameter.Type, argumentSyntaxes);
+                }
+
+                argumentSyntaxes.Add(
+                    new ArgumentSyntaxAndParameterSymbol(
+                        argumentSyntax, 
+                        argumentInfo.Parameter)
+                );
+            }
+
+            var argumentsOfSameTypeByType = argumentSyntaxesByType
+                .Where(argumentsOfSameType => argumentsOfSameType.Value.Count(it => it.Argument.NameColon == null) > 1)
+                .ToList();
+
+            if (!argumentsOfSameTypeByType.Any())
+                return;
+
+            var sbArgumentsOfSameTypeDescriptions = new StringBuilder();
+            var argumentsOfSameTypeSeparator = "";
+            foreach (var argumentsOfSameType in argumentsOfSameTypeByType)
+            {
+                var argumentsOfSameTypeDescription = string.Join(
+                    ", ", 
+                    argumentsOfSameType.Value.Select(it => $"'{it.Parameter.Name}'"));
+
+                sbArgumentsOfSameTypeDescriptions
+                    .Append(argumentsOfSameTypeSeparator)
+                    .Append(argumentsOfSameTypeDescription);
+
+                argumentsOfSameTypeSeparator = " and ";
+            }
+
+            context.ReportDiagnostic(
+                Diagnostic.Create(
+                    Rule, 
+                    invocationExpressionSyntax.GetLocation(), 
+                    messageArgs: new object[] {
+                        methodSymbol.Name, 
+                        sbArgumentsOfSameTypeDescriptions.ToString()
+                    })
+            );
+        }
+    }
+}
